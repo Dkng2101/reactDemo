5c5f925502f5f3c09fe86c2a32ee6f57fd0d4177
DC-VIETIS\DucND <DC-VIETIS\DucND>
2024-10-30 13:54:36 +0700

    add material UI + form header + menu

---
+++ src/components/dashboard/components/AccountHeader.tsx
@@ -0,0 +1,11 @@
+import AccountCircleIcon from "@mui/icons-material/AccountCircle"
+
+export default function AccountHeader() {
+  return (
+    <>
+      <div>
+        <AccountCircleIcon></AccountCircleIcon>Account Setting
+      </div>
+    </>
+  )
+}
---
+++ src/components/dashboard/components/LogOut.tsx
@@ -0,0 +1,12 @@
+import LogoutIcon from "@mui/icons-material/Logout"
+import { Box } from "@mui/material"
+
+export default function LogOut() {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <div>
+        <LogoutIcon></LogoutIcon>Logout
+      </div>
+    </Box>
+  )
+}
